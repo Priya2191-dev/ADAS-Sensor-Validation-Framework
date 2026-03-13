a0327de66b4b0fe834a6fa5d0f6001ba07512ac9
Priya2191-dev <lalepriyanka@gmail.com>
2026-03-13 17:52:16 +0530

    Update aeb_system.py

--- validation/aeb_system.py
+++ validation/aeb_system.py
@@ -1,1 +1,12 @@
+class AEBSystem:
 
+    def __init__(self, threshold=2):
+        self.threshold = threshold
+
+
+    def check_braking(self, ttc):
+
+        if ttc <= self.threshold:
+            return "BRAKE"
+
+        return "NO_BRAKE"
