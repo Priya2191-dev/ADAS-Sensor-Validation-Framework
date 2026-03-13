c5bd1a962278486b6f58e9b6ec2267dfe822ee70
Priya2191-dev <lalepriyanka@gmail.com>
2026-03-13 17:46:09 +0530

    Update collision_warning.py

--- validation/collision_warning.py
+++ validation/collision_warning.py
@@ -1,1 +1,10 @@
+class CollisionWarningSystem:
 
+    def __init__(self, warning_distance=30):
+        self.warning_distance = warning_distance
+
+    def check_warning(self, distance):
+
+        if distance <= self.warning_distance:
+            return "WARNING"
+        return "SAFE"
