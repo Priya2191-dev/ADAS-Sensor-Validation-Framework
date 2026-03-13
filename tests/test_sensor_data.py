d92b07ad10be7f85045474e9dcacd2bc1bcf481d
Priya2191-dev <lalepriyanka@gmail.com>
2026-03-13 18:09:53 +0530

    Update test_sensor_data.py

--- tests/test_sensor_data.py
+++ tests/test_sensor_data.py
@@ -1,1 +1,9 @@
+from validation.sensor_validator import SensorDataValidator
 
+def test_sensor_data_validation():
+
+    validator = SensorDataValidator()
+
+    result = validator.validate_sensor_data("data/sensor_sample_data.csv")
+
+    assert result == True
