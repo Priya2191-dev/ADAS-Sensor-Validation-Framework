f99303cc478277703922d176472787e2ea9527c4
Priya2191-dev <lalepriyanka@gmail.com>
2026-03-13 18:04:29 +0530

    Update test_sensor_fusion.py

--- tests/test_sensor_fusion.py
+++ tests/test_sensor_fusion.py
@@ -1,1 +1,45 @@
+from sensors.radar_sensor import RadarSensor
+from sensors.camera_sensor import CameraSensor
+from validation.sensor_fusion import SensorFusion
 
+
+def test_obstacle_confirmed():
+
+    radar = RadarSensor()
+    camera = CameraSensor()
+    fusion = SensorFusion()
+
+    radar_result = radar.get_object_distance(40)
+    camera_result = camera.detect_object(True)
+
+    result = fusion.fuse(radar_result, camera_result)
+
+    assert result == "Obstacle Confirmed"
+
+
+def test_possible_obstacle_radar_only():
+
+    radar = RadarSensor()
+    camera = CameraSensor()
+    fusion = SensorFusion()
+
+    radar_result = radar.get_object_distance(40)
+    camera_result = camera.detect_object(False)
+
+    result = fusion.fuse(radar_result, camera_result)
+
+    assert result == "Possible Obstacle"
+
+
+def test_no_obstacle():
+
+    radar = RadarSensor()
+    camera = CameraSensor()
+    fusion = SensorFusion()
+
+    radar_result = radar.get_object_distance(200)
+    camera_result = camera.detect_object(False)
+
+    result = fusion.fuse(radar_result, camera_result)
+
+    assert result == "No Obstacle"
