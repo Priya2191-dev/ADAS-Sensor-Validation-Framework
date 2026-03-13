929af07fe2228b6069086d2dfaa41a35adb41f9f
Priya2191-dev <lalepriyanka@gmail.com>
2026-03-13 17:48:16 +0530

    Update sensor_validator.py

--- validation/sensor_validator.py
+++ validation/sensor_validator.py
@@ -1,1 +1,12 @@
+import pandas as pd
 
+class SensorDataValidator:
+
+    def validate_sensor_data(self, file):
+
+        data = pd.read_csv(file)
+
+        if data.isnull().sum().sum() > 0:
+            return False
+
+        return True
