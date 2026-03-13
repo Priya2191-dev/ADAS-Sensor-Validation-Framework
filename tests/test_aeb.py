74d4aab2e092e0782749837fe3fcd81e7797bb2b
Priya2191-dev <lalepriyanka@gmail.com>
2026-03-13 18:14:02 +0530

    Update test_aeb.py

--- tests/test_aeb.py
+++ tests/test_aeb.py
@@ -1,1 +1,19 @@
+from validation.aeb_system import AEBSystem
 
+
+def test_braking_trigger():
+
+    aeb = AEBSystem()
+
+    result = aeb.check_braking(1.5)
+
+    assert result == "BRAKE"
+
+
+def test_no_braking():
+
+    aeb = AEBSystem()
+
+    result = aeb.check_braking(4)
+
+    assert result == "NO_BRAKE"
