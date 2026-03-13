0481cef5a25bdb8f2afb9ac01f79d9f1752f82ad
Priya2191-dev <lalepriyanka@gmail.com>
2026-03-13 13:54:21 +0530

    Update scenario_generator.py

--- simulation/scenario_generator.py
+++ simulation/scenario_generator.py
@@ -1,1 +1,13 @@
+class ScenarioGenerator:
 
+    def front_collision_scenario(self):
+        return {
+            "vehicle_speed": 60,
+            "obstacle_distance": 20
+        }
+
+    def safe_distance_scenario(self):
+        return {
+            "vehicle_speed": 60,
+            "obstacle_distance": 80
+        }
