746152c6fd065152dd8d2e93329c1c718f16b8df
Priya2191-dev <lalepriyanka@gmail.com>
2026-03-13 18:07:41 +0530

    Update test_obstacle_detection.py

--- tests/test_obstacle_detection.py
+++ tests/test_obstacle_detection.py
@@ -1,1 +1,13 @@
+from sensors.lidar_sensor import LidarSensor
+from simulation.obstacle_simulation import ObstacleSimulation
 
+def test_obstacle_detection():
+
+    lidar = LidarSensor()
+    sim = ObstacleSimulation()
+
+    distance = sim.generate_obstacle_distance()
+
+    result = lidar.detect_obstacle(distance)
+
+    assert result in [True, False]
