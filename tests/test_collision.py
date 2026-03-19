939b04f38ed2c32236220ab8c513d93a1bd69ab3
Priya2191-dev <lalepriyanka@gmail.com>
2026-03-19 13:22:54 +0530

    Update test_collision.py

--- tests/test_collision.py
+++ tests/test_collision.py
@@ -1,4 +1,13 @@
 from src.collision_warning import collision_warning
 
-def test_collision():
-    assert collision_warning(3) is True
+def test_collision_true():
+    assert collision_warning(3, 4) is True
+
+def test_collision_false():
+    assert collision_warning(6, 4) is False
+
+def test_collision_equal_boundary():
+    assert collision_warning(4, 4) is False
+
+def test_collision_zero_distance():
+    assert collision_warning(0, 4) is True
