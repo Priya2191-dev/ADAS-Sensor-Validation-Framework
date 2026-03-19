9330d998ee1ebe58d3d02844ed06b172cf39ff93
Priya2191-dev <lalepriyanka@gmail.com>
2026-03-19 13:43:58 +0530

    Create aeb_steps.py

---
+++ features/steps/aeb_steps.py
@@ -0,0 +1,11 @@
+from behave import given, then
+from src.aeb import apply_brakes
+
+@given('TTC is {ttc:f}')
+def step_input(context, ttc):
+    context.ttc = ttc
+
+@then('braking should be {expected}')
+def step_output(context, expected):
+    result = apply_brakes(context.ttc)
+    assert result == (expected == "True")
