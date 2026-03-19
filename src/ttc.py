6991fa162d2a3c16d65665c9b73cdbb6daf21476
Priya2191-dev <lalepriyanka@gmail.com>
2026-03-19 12:11:09 +0530

    Update ttc.py

--- src/ttc.py
+++ src/ttc.py
@@ -1,4 +1,13 @@
 def time_to_collision(distance, speed):
+    """
+    Calculate Time To Collision.
+    """
     if speed == 0:
         return float('inf')
-    return distance / speed
+
+    ttc = distance / speed
+    return ttc
+
+
+if __name__ == "__main__":
+    print(time_to_collision(100, 20))
