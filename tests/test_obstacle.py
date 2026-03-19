0ac202a00f964f0e75cadf3427b5adcfaf52f405
Priya2191-dev <lalepriyanka@gmail.com>
2026-03-19 13:22:11 +0530

    Update test_obstacle.py

--- tests/test_obstacle.py
+++ tests/test_obstacle.py
@@ -1,4 +1,13 @@
 from src.obstacle_detection import detect_obstacles
 
-def test_obstacle():
-    assert detect_obstacles([10,2,3],5) == [2,3]
+def test_detect_obstacles_basic():
+    assert detect_obstacles([10, 2, 3, 7], 5) == [2, 3]
+
+def test_detect_obstacles_no_obstacle():
+    assert detect_obstacles([10, 8, 6], 5) == []
+
+def test_detect_obstacles_all_obstacles():
+    assert detect_obstacles([1, 2, 3], 5) == [1, 2, 3]
+
+def test_detect_obstacles_empty():
+    assert detect_obstacles([], 5) == []
