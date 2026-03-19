30229db697e44b5bdd8b5712a0ed8c3c1ded96bf
Priya2191-dev <lalepriyanka@gmail.com>
2026-03-19 13:38:31 +0530

    Create safety_step.py

---
+++ features/steps/safety_step.py
@@ -0,0 +1,12 @@
+from behave import given, then
+from src.safety_scenario import safety_scenario
+
+@given('speed is {speed:d} and obstacle distance is {distance:d}')
+def step_input(context, speed, distance):
+    context.speed = speed
+    context.distance = distance
+
+@then('safety result should be "{expected}"')
+def step_output(context, expected):
+    result = safety_scenario(context.speed, context.distance)
+    assert result == expected
