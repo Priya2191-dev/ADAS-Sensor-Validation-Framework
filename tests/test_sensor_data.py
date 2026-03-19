94e0412aaec4eb547b2764617fdfbe367bcca175
Priya2191-dev <lalepriyanka@gmail.com>
2026-03-19 13:24:22 +0530

    Update test_sensor_data.py

--- tests/test_sensor_data.py
+++ tests/test_sensor_data.py
@@ -1,4 +1,13 @@
 from src.sensor_data import validate_sensor_data
 
-def test_sensor_data():
-    assert validate_sensor_data([1,2,3]) is True
+def test_valid_data():
+    assert validate_sensor_data([10, 5, 3]) is True
+
+def test_invalid_data_negative():
+    assert validate_sensor_data([10, -1, 3]) is False
+
+def test_empty_data():
+    assert validate_sensor_data([]) is True
+
+def test_all_zero_values():
+    assert validate_sensor_data([0, 0, 0]) is True
