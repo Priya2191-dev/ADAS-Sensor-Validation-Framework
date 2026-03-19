55330f4136729b9d746da0426a9da57f84a12dd8
Priya2191-dev <lalepriyanka@gmail.com>
2026-03-19 13:36:18 +0530

    Create obstacle_steps.py

---
+++ features/steps/obstacle_steps.py
@@ -0,0 +1,15 @@
+from behave import given, when, then
+from src.obstacle_detection import detect_obstacles
+
+@given('sensor data "{data}"')
+def step_sensor_data(context, data):
+    context.data = list(map(int, data.split(',')))
+
+@when('threshold is {threshold:d}')
+def step_threshold(context, threshold):
+    context.result = detect_obstacles(context.data, threshold)
+
+@then('obstacles should be "{expected}"')
+def step_validate(context, expected):
+    expected_list = list(map(int, expected.split(',')))
+    assert context.result == expected_list
