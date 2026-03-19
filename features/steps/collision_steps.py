5ffdd74fb5bb068edee4cc5125934e44927a7a50
Priya2191-dev <lalepriyanka@gmail.com>
2026-03-19 13:37:25 +0530

    Create collision_steps.py

---
+++ features/steps/collision_steps.py
@@ -0,0 +1,11 @@
+from behave import given, then
+from src.collision_warning import collision_warning
+
+@given('distance is {distance:d}')
+def step_distance(context, distance):
+    context.distance = distance
+
+@then('collision warning should be {expected}')
+def step_collision(context, expected):
+    result = collision_warning(context.distance)
+    assert result == (expected == "True")
