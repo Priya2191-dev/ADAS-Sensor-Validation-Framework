60cd12fd5f5718099e05790571608423b168a18b
Priya2191-dev <lalepriyanka@gmail.com>
2026-03-19 13:41:09 +0530

    Create sensor_data_steps.py

---
+++ features/steps/sensor_data_steps.py
@@ -0,0 +1,10 @@
+from behave import given, then
+from src.sensor_data import validate_sensor_data
+
+@given('sensor values "{data}"')
+def step_input(context, data):
+    context.data = list(map(int, data.split(',')))
+
+@then('sensor data should be valid')
+def step_output(context):
+    assert validate_sensor_data(context.data) is True
