397d63be48403752a369f4106ae23ab3d03a56e7
Priya2191-dev <lalepriyanka@gmail.com>
2026-03-19 13:24:57 +0530

    Update test_fusion.py

--- tests/test_fusion.py
+++ tests/test_fusion.py
@@ -1,4 +1,13 @@
 from src.sensor_fusion import sensor_fusion
 
-def test_fusion():
-    assert sensor_fusion([10],[20]) == [15]
+def test_basic_fusion():
+    assert sensor_fusion([10], [20]) == [15]
+
+def test_multiple_values():
+    assert sensor_fusion([10, 20], [20, 30]) == [15, 25]
+
+def test_empty_input():
+    assert sensor_fusion([], []) == []
+
+def test_unequal_length():
+    assert sensor_fusion([10, 20], [30]) == [20]
