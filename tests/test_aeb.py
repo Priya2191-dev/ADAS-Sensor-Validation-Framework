0cdd1681461a993cf80dd06fe0b661916da2f74b
Priya2191-dev <lalepriyanka@gmail.com>
2026-03-19 13:26:16 +0530

    Update test_aeb.py

--- tests/test_aeb.py
+++ tests/test_aeb.py
@@ -1,4 +1,13 @@
 from src.aeb import apply_brakes
 
-def test_aeb():
-    assert apply_brakes(2) is True
+def test_brake_applied():
+    assert apply_brakes(2, 3) is True
+
+def test_brake_not_applied():
+    assert apply_brakes(5, 3) is False
+
+def test_brake_boundary():
+    assert apply_brakes(3, 3) is False
+
+def test_brake_zero_ttc():
+    assert apply_brakes(0, 3) is True
