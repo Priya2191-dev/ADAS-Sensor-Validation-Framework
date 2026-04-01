68e28b6594a07ddb93e2c1d94c6028e31ee499ae
Priya2191-dev <lalepriyanka@gmail.com>
2026-04-01 10:54:55 +0530

    Update obstacle_steps.py

--- features/steps/obstacle_steps.py
+++ features/steps/obstacle_steps.py
@@ -7,18 +7,19 @@ def parse_input(data):
         return []
     return list(map(float, data.split(",")))
 
-
 @given('sensor data "{data}"')
-def step_given_sensor_data(context, data):
+def given_sensor_data(context, data):
     context.sensor_data = parse_input(data)
 
+@given('sensor data')
+def given_empty_sensor_data(context):
+    context.sensor_data = []
 
-@when('threshold is {threshold:g}')
-def step_when_threshold(context, threshold):
+@when('threshold is {threshold}')
+def when_threshold(context, threshold):
     context.result = detect_obstacles(context.sensor_data, threshold)
 
-
 @then('obstacles should be "{expected}"')
-def step_then_obstacles(context, expected):
+def then_obstacles(context, expected):
     expected_list = parse_input(expected)
     assert context.result == expected_list
