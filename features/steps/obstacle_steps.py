35e919b28053a79ac8ffd179bd3faaf96e629050
Priya2191-dev <lalepriyanka@gmail.com>
2026-04-01 10:47:03 +0530

    Update obstacle_steps.py

--- features/steps/obstacle_steps.py
+++ features/steps/obstacle_steps.py
@@ -1,15 +1,24 @@
 from behave import given, when, then
 from obstacle_detection import detect_obstacles
 
-@given('sensor data "{data}"')
-def step_sensor_data(context, data):
-    context.data = list(map(int, data.split(',')))
 
-@when('threshold is {threshold:d}')
-def step_threshold(context, threshold):
-    context.result = detect_obstacles(context.data, threshold)
+def parse_input(data):
+    if data.strip() == "":
+        return []
+    return list(map(float, data.split(",")))
+
+
+@given('sensor data "{data}"')
+def step_given_sensor_data(context, data):
+    context.sensor_data = parse_input(data)
+
+
+@when('threshold is {threshold:g}')
+def step_when_threshold(context, threshold):
+    context.result = detect_obstacles(context.sensor_data, threshold)
+
 
 @then('obstacles should be "{expected}"')
-def step_validate(context, expected):
-    expected_list = list(map(int, expected.split(',')))
+def step_then_obstacles(context, expected):
+    expected_list = parse_input(expected)
     assert context.result == expected_list
