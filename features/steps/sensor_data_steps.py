0546cc3bf2bb27ce7a77f5e71056fa910608639c
Priya2191-dev <lalepriyanka@gmail.com>
2026-04-01 17:38:56 +0530

    Update sensor_data_steps.py

--- features/steps/sensor_data_steps.py
+++ features/steps/sensor_data_steps.py
@@ -1,10 +1,28 @@
-from behave import given, then
+from behave import given, when, then
 from sensor_data import validate_sensor_data
 
+
+# Helper function
+def convert_to_list(data):
+    #If empty string -> return empty list
+    if data.strip() == "":
+        return []
+
+    #Convert "10, 5, 3" into [10.0, 5.0, 3.0]
+    return [float(x) for x in data.split(",")]
+
 @given('sensor values "{data}"')
-def step_input(context, data):
-    context.data = list(map(int, data.split(',')))
+def given_data(context, data):
+    context.data = convert_to_list(data)
+
+@when('system validates sensor data')
+def when_validate(context):
+    context.result = validate_sensor_data(context.data)
 
 @then('sensor data should be valid')
-def step_output(context):
-    assert validate_sensor_data(context.data) is True
+def then_valid(context):
+    assert context.result is True
+
+@then('sensor data should be invalid')
+def then_invalid(context):
+    assert context.result is False
