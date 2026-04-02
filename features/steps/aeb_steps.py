f0c209a9ab3576f264de319f2c8df3a3a8ef387f
Priya2191-dev <lalepriyanka@gmail.com>
2026-04-02 17:00:47 +0530

    Update aeb_steps.py

--- features/steps/aeb_steps.py
+++ features/steps/aeb_steps.py
@@ -2,15 +2,17 @@ from behave import given, when, then
 from aeb import apply_brakes
 
 
-@given('AEB TTC is {ttc:f}')
-def step_given_ttc(context, ttc):
-    context.ttc = ttc
+@given('AEB TTC is {ttc}')
+def given_ttc(context, ttc):
+    context.ttc = float(ttc)  
+
 
 @when('system evaluates braking')
-def step_when_eval(context):
+def when_evaluate(context):
     context.result = apply_brakes(context.ttc)
 
-@then('braking should be "{expected}"')
-def step_then_output(context, expected):
+
+@then('braking should be {expected}')
+def then_output(context, expected):
     expected_bool = expected.lower() == "true"
     assert context.result == expected_bool
